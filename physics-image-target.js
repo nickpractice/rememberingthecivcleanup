ac4fe861857b16af4f24135bd07f979198cd118f
nickpractice <nickbbigalke@gmail.com>
2021-07-08 15:12:50 -0700

    Pos0.1.0Sc2.0Js160.0

--- physics-image-target.js
+++ physics-image-target.js
@@ -28,7 +28,7 @@ AFRAME.registerComponent('physics-image-target', {
           property: 'scale',
           dur: 1500,
           from: '0.001 0.001 0.001',
-          to: '130.0 130.0 130.0',
+          to: '160.0 160.0 160.0',
           easing: 'easeInOutQuad',
         })
         // if (!treeShook) {
